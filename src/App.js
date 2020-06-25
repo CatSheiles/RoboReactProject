46ac83729bc727a40b01a8ae98912c05f7427268
CatSheiles <cathysheiles@gmail.com>
2020-06-25 15:14:16 +1000

    update cards and add styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//STATE describes app and changes value of searchbox and input
+//app owns state which can change robots - love syntax
+//checkout react.component lifecycle hooks
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => this.setState({ robots: users}));
+    }
+
+    //when you make your own methods use error functions
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+    render() {
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+//if you had hundreds/thousands of users you can do this to speed reaction time up
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                    <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
